4b7bedb8d843311f69ba287d0ce1c6fd63665dc8
shishanee <mansur72095@gmail.com>
2023-10-03 19:17:14 +0300

    code review

--- src/components/CurrentPage/CurrentPage.jsx
+++ src/components/CurrentPage/CurrentPage.jsx
@@ -3,7 +3,6 @@ import { Pagination } from "antd";
 import styles from "./CurrentPage.module.scss";
 
 const CurrentPage = ({ itemsPerPage, items, setCurrentPage, currentPage }) => {
-  const totalPages = Math.ceil(items.length / itemsPerPage);
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
